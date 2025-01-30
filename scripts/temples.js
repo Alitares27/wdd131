c8a7fa898ceb80464f05397cc5335f77c889a8c8
Alitares27 <aldair2795@hotmail.com>
2025-01-30 01:31:07 -0300

    Enhance filtered-temples.html and temples.js for improved navigation and accessibility; update image URLs and styles for better display

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,4 @@
-/* Get Dates*/
+// Get Dates
 const currentYear = new Date().getFullYear();
 document.getElementById("year").innerHTML = currentYear;
 
@@ -22,7 +22,7 @@ const temples = [
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/800x500/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
   },
   {
     templeName: "Manti Utah",
@@ -30,7 +30,7 @@ const temples = [
     dedicated: "1888, May, 21",
     area: 74792,
     imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/800x500/manti-temple-766504-wallpaper.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
   },
   {
     templeName: "Payson Utah",
@@ -38,7 +38,7 @@ const temples = [
     dedicated: "2015, June, 7",
     area: 96630,
     imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/800x500/payson-utah-temple-daylight-1416668-wallpaper.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
   },
   {
     templeName: "Yigo Guam",
@@ -46,7 +46,7 @@ const temples = [
     dedicated: "2020, May, 2",
     area: 6861,
     imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/800x500/yigo_guam_temple_4.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
   },
   {
     templeName: "Washington D.C.",
@@ -54,7 +54,7 @@ const temples = [
     dedicated: "1974, November, 19",
     area: 156558,
     imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
   },
   {
     templeName: "Lima Perú",
@@ -62,7 +62,7 @@ const temples = [
     dedicated: "1986, January, 10",
     area: 9600,
     imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
   },
   {
     templeName: "Mexico City Mexico",
@@ -92,11 +92,43 @@ const temples = [
   },
 ];
 
-//Display Temples function
-function displayTemples() {
-  const container = document.querySelector(".container");
+//Links
+const homeLink = document.getElementById("home");
+const oldLink = document.getElementById("old");
+const newLink = document.getElementById("new");
+const largeLink = document.getElementById("large");
+const smallLink = document.getElementById("small");
 
-  temples.forEach(temple => {
+
+const container = document.querySelector(".container");
+
+//Event Listeners
+homeLink.addEventListener("click", ()=>{
+  displayTemples(temples);
+});
+
+oldLink.addEventListener("click", ()=>{
+  displayTemples(temples.filter(temple => temple.dedicated < "1900"));
+});
+
+newLink.addEventListener("click", ()=>{
+  displayTemples(temples.filter(temple => temple.dedicated > "2000"));
+});
+
+largeLink.addEventListener("click", ()=>{
+  displayTemples(temples.filter(temple => temple.area > "90000"));
+});
+
+smallLink.addEventListener("click", ()=>{
+  displayTemples(temples.filter(temple => temple.area < "10000"));
+});
+
+displayTemples(temples);
+
+//Display Temples function
+function displayTemples(filteredTemples) {
+  container.innerHTML = "";
+  filteredTemples.forEach(temple => {
     const card = document.createElement("div");
     card.className = "card";
 
@@ -109,16 +141,16 @@ function displayTemples() {
     image.setAttribute("height", "400");
 
     const name = document.createElement("h2");
-    name.textContent = temple.templeName;
+    name.innerHTML = temple.templeName;
 
     const location = document.createElement("p");
-    location.textContent = `Location: ${temple.location}`;
+    location.innerHTML = `<span class="label">Location: </span>${temple.location}`;
 
     const dedication = document.createElement("p");
-    dedication.textContent = `Dedicated: ${temple.dedicated}`;
+    dedication.innerHTML = `<span class="label">Dedicated: </span>${temple.dedicated}`;
 
     const area = document.createElement("p");
-    area.textContent = `Size: ${temple.area} sq ft`;
+    area.innerHTML = `<span class="label">Size: </span>${temple.area.toLocaleString('es-ES')} sq ft`;
 
     card.appendChild(name);
     card.appendChild(location);
@@ -128,5 +160,5 @@ function displayTemples() {
     container.appendChild(card);
   });
 }
-displayTemples();
+
 
